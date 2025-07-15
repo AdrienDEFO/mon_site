e88a57fe4c0ea9f9ea90644009068c61f729fd94
AdrienDEFO <adriendefo@gmail.com>
2025-07-15 20:56:05 +0100

    Corrige le chemin des images

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (photoPage) {
     const imageNames = [
       "photo1.jpg", "photo2.jpg", "photo3.jpg", "photo4.jpg",
-      "photo5.jpg", "photo6.jpg", "photo7.jpg", "photo8.jpg"
+      "photo5.jpg", "photo6.jpg", "photo7.jpg"
     ];
 
     const gallery = document.getElementById("photo-gallery");
